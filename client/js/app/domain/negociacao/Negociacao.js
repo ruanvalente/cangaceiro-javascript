3acba9eec6f3244240480c1e95534a80d1d97396
Ruan Valente <ruanopensuselinux@gmail.com>
2019-06-25 13:46:19 -0300

    cap-07 terminado.

--- client/js/app/domain/negociacao/Negociacao.js
+++ client/js/app/domain/negociacao/Negociacao.js
@@ -1,10 +1,6 @@
 class Negociacao {
   constructor(_data, _quantidade, _valor) {
-    Object.assign(this, {
-      _quantidade: _quantidade,
-      _valor: _valor
-    });
-
+    Object.assign(this, { _quantidade, _valor });
     this._data = new Date(_data.getTime());
     Object.freeze(this);
   }
